defbb3316be0d6a2da611d7fa2b76bd70c707c4b
김용민 <dydals3440@gmail.com>
2024-07-10 01:46:47 +0900

    feat: 게시글 수정/삭제 구현

--- src/post/post.controller.ts
+++ src/post/post.controller.ts
@@ -1,4 +1,16 @@
-import { Body, Controller, Get, Post, Query } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  Param,
+  ParseIntPipe,
+  Patch,
+  Post,
+  Query,
+  UsePipes,
+  ValidationPipe,
+} from '@nestjs/common';
 import { PostService } from './post.service';
 import { CreatePostDto } from './dto/create-post.dto';
 
@@ -11,8 +23,30 @@ export class PostController {
     return this.postService.getPosts(page);
   }
 
+  @Get('/posts/:id')
+  // Pipe를 사용해서, Id가 string으로와도, parseInt형태로 바꿀 수 있음.
+  getPost(@Param('id', ParseIntPipe) id: number) {
+    return this.postService.getPostById(id);
+  }
+
   @Post('/posts')
+  @UsePipes(ValidationPipe)
   createPost(@Body() createPostDto: CreatePostDto) {
     return this.postService.createPost(createPostDto);
+  }
+
+  @Delete('/posts/:id')
+  deletePost(@Param('id', ParseIntPipe) id: number) {
+    return this.postService.deletePost(id);
+  }
+
+  @Patch('/posts/:id')
+  @UsePipes(ValidationPipe)
+  updatePost(
+    @Param('id', ParseIntPipe) id: number,
+    @Body() // 수정할떄는 좌표를 수정하지 않음. 주소랑
+    updatePostDto: Omit<CreatePostDto, 'longitude' | 'latitude' | 'address'>,
+  ) {
+    return this.postService.updatePost(id, updatePostDto);
   }
 }
--- src/post/post.service.ts
+++ src/post/post.service.ts
@@ -1,4 +1,8 @@
-import { Injectable, InternalServerErrorException } from '@nestjs/common';
+import {
+  Injectable,
+  InternalServerErrorException,
+  NotFoundException,
+} from '@nestjs/common';
 import { CreatePostDto } from './dto/create-post.dto';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Post } from './post.entity';
@@ -10,6 +14,7 @@ export class PostService {
     @InjectRepository(Post)
     private postRepository: Repository<Post>,
   ) {}
+
   async getPosts(page: number) {
     const perPage = 10;
     const offset = (page - 1) * perPage;
@@ -19,6 +24,29 @@ export class PostService {
       .take(perPage)
       .skip(offset)
       .getMany();
+  }
+
+  async getPostById(id: number) {
+    try {
+      const foundPost = await this.postRepository
+        // post table에 있는 포스트
+        .createQueryBuilder('post')
+        // 어떤 포스트냐면
+        // where를 이용하면, post.id가 인자로 받는 id와 같은 id를 찾을 것.
+        .where('post.id = id', { id })
+        // getMany가 아닌 getOne()
+        .getOne();
+
+      if (!foundPost) {
+        throw new NotFoundException('존재하지 않는 피드입니다.');
+      }
+
+      return foundPost;
+    } catch (error) {
+      throw new InternalServerErrorException(
+        '장소를 추가하는 도중 에러가 발생했습니다.',
+      );
+    }
   }
 
   async createPost(createPostDto: CreatePostDto) {
@@ -53,6 +81,54 @@ export class PostService {
         '장소를 추가하는 도중 에러가 발생했습니다.',
       );
     }
+    return post;
+  }
+
+  async deletePost(id: number) {
+    try {
+      const result = await this.postRepository
+        .createQueryBuilder('post')
+        .delete()
+        .from(Post)
+        // id가 인자와 똑같은 아이디를 삭제
+        .where('id = :id', { id })
+        .execute();
+      // 결과가 일어나지 않으면
+      if (result.affected === 0) {
+        throw new NotFoundException('존재하지 않는 피드입니다.');
+      }
+      return id;
+    } catch (error) {
+      console.log(error);
+      throw new InternalServerErrorException(
+        '삭제하는 도중 에러가 발생했습니다.',
+      );
+    }
+  }
+
+  async updatePost(
+    id: number,
+    updatePostDto: Omit<CreatePostDto, 'longitude' | 'latitude' | 'address'>,
+  ) {
+    const post = await this.getPostById(id);
+
+    const { title, description, color, date, score, imageUris } = updatePostDto;
+    post.title = title;
+    post.description = description;
+    post.color = color;
+    post.date = date;
+    post.score = score;
+
+    // image module
+    try {
+      await this.postRepository.save(post);
+    } catch (error) {
+      console.log(error);
+      throw new InternalServerErrorException(
+        '삭제하는 도중 에러가 발생했습니다.',
+      );
+    }
+
     return post;
   }
 }
